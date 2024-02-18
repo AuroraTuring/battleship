120a365d26ba1d00822b8069ac61cd4466e1d6e9
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-17 18:59:19 -0700

    Describe #valid_coordinate

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -20,6 +20,19 @@ RSpec.describe Board do
     end
   end
 
+  describe "#valid_coordinate?" do
+    it "knows if a coordinate is on the board" do
+      expect(@board.valid_coordinate?("A1")).to be true
+      expect(@board.valid_coordinate?("D4")).to be true
+    end
+
+    it "knows if a coordinate is not on the board" do
+      expect(@board.valid_coordinate?("A5")).to be false
+      expect(@board.valid_coordinate?("E1")).to be false
+      expect(@board.valid_coordinate?("A22")).to be false
+    end
+  end
+
 
 
 end
