10559ac096493d067cde1abba718a734a1195e2d
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-17 18:57:43 -0700

    Fix single quotes to be double quotes

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,19 +1,19 @@
 require "./spec/spec_helper"
 
 RSpec.describe Board do
-  before 'each' do
+  before "each" do
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
   end
 
   describe "#initialize" do
-    it 'exists' do
+    it "exists" do
       expect(@board).to be_a(Board)
       expect(@cruiser).to be_a(Ship)
     end
 
-    it 'initializes Board with cells' do
+    it "initializes Board with cells" do
       @board.cells.each_value do |cell|
         expect(cell).to be_instance_of(Cell)
       end
