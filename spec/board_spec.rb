1a4c78b938cbd6c1ab3de4b2a329b78c5bddf60d
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-17 19:04:35 -0700

    Describe #place_ship

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -34,14 +34,14 @@ RSpec.describe Board do
   end
 
   describe "#valid_placement?" do
-  it "ship length" do
+  it "knows ship length" do
     expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be false
     expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A3"])).to be true
     expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be false
     expect(@board.valid_placement?(@submarine, ["A2", "A3"])).to be true
   end
 
-  it "consecutive_placement" do
+  it "has consecutive placement" do
     expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be true #vertical
     expect(@board.valid_placement?(@cruiser, ["B1", "B2", "B3"])).to be true #horizontal
     expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be false #diagonal
@@ -49,6 +49,29 @@ RSpec.describe Board do
     expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be false #not consecutive
   end
 end
+
+  describe "#place_ship" do
+    it "can place ship" do
+      @board.place(@cruiser, ["A1", "A2", "A3"])
+
+      @cell_1 = @board.cells["A1"]
+      @cell_2 = @board.cells["A2"]
+      @cell_3 = @board.cells["A3"]
+
+      expect(@cell_1.ship).to eq(@cruiser)
+      expect(@cell_2.ship).to eq(@cruiser)
+      expect(@cell_3.ship).to eq(@cruiser)
+      expect(@cell_3.ship == @cell_2.ship).to eq(true)
+    end
+
+    it "knows if a ship is overlapping" do
+      @cell_1 = @board.cells["A1"]
+      @cell_2 = @board.cells["A2"]
+      @cell_3 = @board.cells["A3"]
+      @board.place(@cruiser, ["A1", "A2", "A3"])
+      expect(@board.valid_placement?(@submarine, ["A1", "B1"])).to be false
+    end
+  end
 
 
 end
