c79cd50cc07d3ea67a9afb6b2a47ef47d3b92f1c
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-17 19:01:36 -0700

    Describe #valid_placement

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -33,6 +33,22 @@ RSpec.describe Board do
     end
   end
 
+  describe "#valid_placement?" do
+  it "ship length" do
+    expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be false
+    expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A3"])).to be true
+    expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be false
+    expect(@board.valid_placement?(@submarine, ["A2", "A3"])).to be true
+  end
+
+  it "consecutive_placement" do
+    expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be true #vertical
+    expect(@board.valid_placement?(@cruiser, ["B1", "B2", "B3"])).to be true #horizontal
+    expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be false #diagonal
+    expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be false #descending
+    expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be false #not consecutive
+  end
+end
 
 
 end
