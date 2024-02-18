600e628f1fceb67d0768c73e96a2cd65c979d0b4
NeilTheSeal <neil.hendren@gmail.com>
2024-02-17 18:14:26 -0700

    Add cells initialization

--- lib/board.rb
+++ lib/board.rb
@@ -4,5 +4,10 @@ class Board
 
   def initialize
     @cells = {}
+    ("A".."D").each do |row|
+      (1..4).each do |column|
+        @cells["#{row}#{column}"] = Cell.new("#{row}#{column}")
+      end
+    end
   end
 end
