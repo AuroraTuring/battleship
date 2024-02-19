449dc08add58654392804425a8f70ea1ec79bac2
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-18 20:28:53 -0700

    Add first three methods and pass

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,24 @@
 class Turn
 
+  def initialize(player_board, computer_board)
+    @player_board = player_board
+    @computer_board = computer_board
+  end
 
+  def display_both_boards
+    display_player_board +
+    display_computer_board
+  end
 
+  def display_player_board
+    "==============PLAYER BOARD==============\n" +
+    @player_board.render(true)
+  end
+
+  def display_computer_board
+    "==============COMPUTER BOARD==============\n" +
+    @computer_board.render
+  end
 
 
 end
