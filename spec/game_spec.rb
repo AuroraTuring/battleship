060cbc2f50bc36ff548affa6e32da0d5274b5c79
NeilTheSeal <neil.hendren@gmail.com>
2024-02-18 19:45:19 -0700

    Add tests for initialization

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Game do
   before(:each) do
     @game = Game.new
   end
-end
+
+  describe "#initialization" do
+    it "exists" do
+      expect(@game).to be_an_instance_of(Game)
+    end
+    it "initializes attributes" do
+      expect(@game.player_board).to eq(nil)
+      expect(@game.computer_board).to eq(nil)
+      expect(@game.game_in_progress).to eq(false)
+    end
+  end
