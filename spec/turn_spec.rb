156f4528b2863df795627f54b1039b3353968607
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-18 20:10:38 -0700

    Complete #display_boards

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -21,11 +21,47 @@ RSpec.describe Turn do
 
   describe "#display_boards" do
     it "can dislay player board" do
-
+      @player_board.place(@cruiser, ["A1", "A2", "A3"])
+      player_board = @turn.display_player_board
+      expected_output = "==============PLAYER BOARD==============/n" +
+                        "  1 2 3 4 /n" +
+                        "A S S S . /n" +
+                        "B . . . . /n" +
+                        "C . . . . /n" +
+                        "D . . . . /n"
+      expect(player_board).to eq(expected_output)
     end
 
     it "can display computer board" do
+      @computer_board.place(@submarine, ["B1", "B2"])
+      computer_board = @turn.display_computer_board
+      expected_output = "==============COMPUTER BOARD==============/n" +
+                        "  1 2 3 4 /n" +
+                        "A . . . . /n" +
+                        "B . . . . /n" +
+                        "C . . . . /n" +
+                        "D . . . . /n"
+      expect(computer_board).to eq(expected_output)
+    end
 
+    it "can display both boards" do
+      @player_board.place(@cruiser, ["A1", "A2", "A3"])
+      @computer_board.place(@submarine, ["B1", "B2"])
+
+      expected_output = "==============PLAYER BOARD==============/n" +
+                        "  1 2 3 4 /n" +
+                        "A S S S . /n" +
+                        "B . . . . /n" +
+                        "C . . . . /n" +
+                        "D . . . . /n" +
+
+                        "==============COMPUTER BOARD==============/n" +
+                        "  1 2 3 4 /n" +
+                        "A . . . . /n" +
+                        "B . . . . /n" +
+                        "C . . . . /n" +
+                        "D . . . . /n"
+      expect(@turn.display_both_boards).to eq(expected_output)
     end
   end
 
